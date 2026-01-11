734099982214fed15a39d7a2add6801dde9ccd87
Sahil Goyal <itzsahilg1@gmail.com>
2026-01-11 22:44:25 +0530

    Delete backend directory

--- backend/src/routes/chartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { generateChart } = require('../controllers/chartController');
-const router = express.Router();
-
-// Route for generating charts
-router.post('/generate', generateChart);
-
-module.exports = router;
